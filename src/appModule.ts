53c91341e2c51ca17b2998949ae28510f7309ad9
Leandro Moscheni <leandromoscheni@gmail.com>
2021-04-29 22:13:33 -0300

    Adding Swagger, Config and logs on server start

--- src/appModule.ts
+++ src/appModule.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { HealthCheckController } from './HealthCheck/healthCheckController';
 import { HealthCheckFacade } from './HealthCheck/healthCheckFacade';
 
+import configuration from '../config';
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ load: [configuration] })],
   controllers: [HealthCheckController],
   providers: [HealthCheckFacade],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './appModule';
+
+import pkg from '../package.json';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const log = new Logger('bootstrap');
+
+  const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle(pkg.name)
+    .setDescription(pkg.description)
+    .setVersion(pkg.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/doc', app, document);
+
+  const port = configService.get('port');
+
+  await app.listen(port).then(() => {
+    log.debug(`Server is running on port ${port} | http://localhost:${port}`);
+    log.debug(`Healt Check | http://localhost:${port}/api/health-check`);
+    log.debug(`Swagger | http://localhost:${port}/doc`);
+  });
 }
 bootstrap();
